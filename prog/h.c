320cdd7da4bd5ad4fd020f083af2b51444ed4bb4
asaprocky2011 <57670264+asaprocky2011@users.noreply.github.com>
2021-11-12 02:13:44 +0300

    вторая лаба

--- prog/h.c
+++ prog/h.c
@@ -1,69 +1,251 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main(int argc, char* argv[]) {
-    freopen(argv[2], "r", stdin);
-    if ((strcmp(argv[1], "-l") == 0) || (strcmp(argv[1], "--lines") == 0))
+
+const int base = 100000000;
+
+typedef struct
+{
+    int a[40];
+} uint1024_t;
+ 
+ uint1024_t from_uint(unsigned int x)
+{
+    uint1024_t b;
+    int i = 0;
+    if (x == 0)
     {
-        int n = 0;
-        int str = getchar();
-        int str1;
-        if (str == EOF) 
+        b.a[i] = 0;
+        i++;
+        while (i != 40)
         {
-            printf("incorect file name or file is empty");
-        }
-        else
-        {
-            while (str != EOF)
-            {
-                str1 = str;
-                if (str == '\n') n += 1;
-                str = getchar();
-            }
-        printf("%d", n + 1);
+            b.a[i] = -1;
+            i++;
         }
     }
-    else if ((strcmp(argv[1], "-w") == 0) || (strcmp(argv[1], "--words") == 0))
-    {
-        int n = 0, isword = 0;
-        int str = getchar();
-        if (str == EOF) printf("incorect file name or file is empty");
-        else
+    else
+        while (i != 40)
         {
-        while (str != EOF)
-        {
-            if ((str == '\n' || str == ' ' || str == '\t')  && isword == 1) 
+            if (x != 0)
             {
-                isword = 0;
-                n += 1;
+                b.a[i] = x % base;
+                x /= base;
             }
-            else if (str != '\n' && str != ' ' && str != '\t') 
+            else 
             {
-                isword = 1;
+                b.a[i] = -1;  
             }
-            str = getchar();
+            i++;
         }
+    return b;
+}
+
+void printf_value(uint1024_t x)
+{
+    int proof = 0;
+    for (int i = 0; i <= 39; i++)
+        if (x.a[i] != -1 && x.a[i] != 0){
+            proof = 1;
+            break;
+        }
+    if (proof == 1)
+    {
+    int flag = 1;
+    for (int i = 39; i>=0; i--)
+        if (x.a[i] != -1)
+        {
+            if (flag != 1)
+            {
+                int len_of_digit;
+                if (x.a[i] == 0)
+                    len_of_digit = 1;
+                else
+                    len_of_digit = floor(log10(abs(x.a[i]))) + 1;
+                for (int j = 0; j < (8 - len_of_digit); j++)
+                    printf ("0");
+                printf ("%d", x.a[i]);
+            }
+            else
+            {
+                flag = 0;
+                if (x.a[i] != 0)
+	                printf ("%d", x.a[i]);
+            }
+        }
+    }
+    else
+        printf("0");
+
+}
+
+uint1024_t subtr_op(uint1024_t x, uint1024_t y) 
+{
+    int flag = 0;
+    for (int i = 0; i <= 39; i++)
+    {
+        if(flag == 1)
+        {
+            if (x.a[i] >= 1)
+            {
+                x.a[i] -= 1;
+                flag = 0;
+            }
+            else
+            {
+                x.a[i] += 99999999;
+            }
+        }
+        if (x.a[i] != -1 && y.a[i] != -1)
+        {
+	        if (x.a[i] >= y.a[i])
+            {
+                x.a[i] -= y.a[i];
+            }
+            else
+            {
+                x.a[i] += base - y.a[i];
+                flag = 1;
+            }
+                
+        }
+    }
+    return x;
+}
+
+uint1024_t add_op(uint1024_t x, uint1024_t y)
+{
+    int flag = 0;
+    for (int i = 0; i <= 39; i++)
+    {
+            if (x.a[i] != -1 && y.a[i] != -1)
+            {
+	            x.a[i] += y.a[i];
+                if (flag == 1)
+                {
+                    x.a[i] += 1;
+                    flag = 0;
+                }
+                if (x.a[i] / base >= 1)
+                {
+                    flag = 1;
+                    x.a[i] = x.a[i] % base;
+                }
+            }
+            else if (x.a[i] + y.a[i] > -2)
+            {
+                if (x.a[i] < y.a[i])
+                {
+                    x.a[i] = y.a[i];    
+                }
+                if (flag == 1)
+                    {
+                    x.a[i] += 1;
+                    flag = 0;
+                    }
+                if (x.a[i] / base >= 1)
+                    {
+                    flag = 1;
+                    x.a[i] = x.a[i] % base;
+                    }
+                    
+            }
+            else
+            {
+                if (flag == 1)
+                    x.a[i] = 1;
+                    flag = 0;
+            }
+    }
+    return x;
+}
+
+void scanf_value(uint1024_t *x)
+{
+    char *str;
+    str = (char *)(malloc(315 * sizeof(char)));
+    scanf("%s", str);
+    for (int i = 0; i <= 39; i++)
+    {
+        x->a[i] = -1;
+    }
+    int n = strlen(str);
+    for (int i = 0; i <= 39; i++)
+    {
+        char substring[8] = "00000000";
+        int flag = 0;
+        for (int j = 7; j >= 0; j--)
+        {
+            if (n >= 1)
+            {
+                flag = 1;
+                substring[j] = str[n - 1];
+                n -= 1;
+            }
+            else
+                break;
+
+        }
+        if (flag == 1)
+        {
+            x -> a[i] = (int) atoi (substring);
+        }
+
+    }
+}
+
+uint1024_t mult_op(uint1024_t x, uint1024_t y)
+{
+    uint1024_t z;
+    for (int i = 0; i <= 39; i++)
+    {
+        z.a[i] = 0;
+    }
+     
+    for (int i = 0; i <= 39; i++)
+    {
+        long long remains = 0;
+        for (int j = 0; i + j <= 39; j++)
+        {
+            if (x.a[i] != -1 && y.a[j] != -1){
+            long long res = (long long)(z.a[i + j]) + ((long long)(x.a[i]) * (long long)(y.a[j])) + remains;
+            z.a[i + j] = (int)(res % (long long)(base));
+            remains = (long long)(res / base);
+            }
+            else if (remains != 0)
+            {
+                z.a[i + j] = (int)(remains);
+                remains = 0;
+            }
+
+        }
+    }
+    for (int i = 39; i >= 0; i--)
+    {
+        if (z.a[i] == 0)
+            z.a[i] = -1;
+        else
+            break;
+    }
         
-        if (isword == 1) n +=1;  
-        printf("%d", n);
-        }
-    }
-    else if ((strcmp(argv[1], "-c") == 0) || (strcmp(argv[1], "--bytes") == 0))
-    {
-        int str = getchar(), n = 0;
-        if (str == EOF) printf("incorect file name or file is empty");
-        else
-        {
-        while (str != EOF)
-        {
-            if (str == '\n') n += 2;
-            else n += 1;
-            str = getchar();
-        }
-        printf("%d", n);
-        }
-    }
-    else printf("incorrect option name");
-    fclose(stdin);
+    return z;
+}
+
+int main() 
+{
+    uint1024_t a;
+    uint1024_t b, v;
+    scanf_value(&a);
+    scanf_value(&b);
+    printf("multiplication: ");
+    v = mult_op(a, b);
+    printf_value(mult_op(a, b));
+    printf("\ndifference: ");
+    printf_value(subtr_op(a, b));
+    printf("\namount: ");
+    printf_value(add_op(a, b));
     return 0;
 }
+
